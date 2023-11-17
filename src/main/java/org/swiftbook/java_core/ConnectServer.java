4a952b09d464620ec899aa3e206a3bf93b065fc4
Shapoval Denis <speleo04@yandex.ru>
2023-11-18 01:26:58 +0300

    semaphore

--- src/main/java/org/swiftbook/java_core/ConnectServer.java
+++ src/main/java/org/swiftbook/java_core/ConnectServer.java
@@ -1,21 +1,19 @@
 package org.swiftbook.java_core;
 
-import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ConnectServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(200);
         Connection connection = Connection.getConnection();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 200; i++) {
             executorService.submit(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        connection.doWork();
+                        connection.work();
                     } catch (InterruptedException e) {
                         throw new RuntimeException(e);
                     }
@@ -23,24 +21,35 @@ public class ConnectServer {
             });
         }
         executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
 
     }
 }
 class Connection{
     private static Connection connection = new Connection();
     private int connectionsCount;
+    Semaphore semaphore = new Semaphore(10);
     private Connection(){};
 
     public static Connection getConnection(){
         return connection;
     }
 
-    public void doWork() throws InterruptedException {
+    public void work() throws InterruptedException {
+        semaphore.acquire(); //останавливаем 11 поток, ждет пока отработает semaphore.release();
+        try {
+            doWork(); // работаем с ограниченным ресурсом в приватном методе, чтоб не было возможности его вызвать без семафора
+        } finally {
+            semaphore.release();//finally чтоб в любом случае мы вернули разрешение на работу с ресурсом в семафор, даже при исключении при коннекте с сервером.
+        }
+    }
+
+    private void doWork() throws InterruptedException {
         synchronized (this){
             connectionsCount++;
             System.out.println("ConnectionsCount " + connectionsCount);
         }
-        Thread.sleep(5000);
+        Thread.sleep(1000);
 
         synchronized (this){
             connectionsCount--;
